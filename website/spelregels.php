c4b0c26ae435d7aa881b76b7f773506bc7707426
Rowan van laar <534571@edu.rocmn.nl>
2019-04-04 09:25:43 +0200

    website files toegevoegd

---
+++ website/spelregels.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+    <ul class="nav">
+        <li><a class="navigatie" href="home.php">Home</a></li>
+        <li><a class="navigatie" href="blackjack.php">Blackjack</a></li>
+        <li><a class="navigatie" href="breakout.php">Breakout</a></li>
+        <li><a class="navigatie" href="contact.php">Contact</a></li>
+        <li><a class="navigatie" id="actief" href="spelregels.php">Spelregels</a></li>
+        <img class="logo" src="images/logo.png" alt="blije dobbelsteen" height="50" width="50">
+    </ul>
+</body>
+<footer>
+    <div class="container">
+        <div class="container-1">
+            <div class="title-footer">Games:</div>
+            <ul class="foot">
+                <li><a class="footer" href="blackjack.php">Blackjack</a></li>
+                <li><a class="footer" href="Breakout.php">Breakout</a></li>
+            </ul>
+        </div>
+        <div class="container-1">
+            <div class="title-footer">Volg ons:</div>
+            <div>
+                <a class="facebook" href="https://nl-nl.facebook.com/blije.dobbelsteen.9">
+                    <img alt="facebook" src="images/facebook.jpg" width="90" height="50">
+                </a>
+            </div>
+        </div>
+        <div class="container-2">
+            <img src="images/logo.png" alt="blije dobbelsteen" width="100" height="100">
+        </div>
+    </div>
+</footer>
+</html>
